1b810f170c7ae67aca170c6445b3a5180f0d6ef1
reasonly8 <reasonly8@sina.com>
2024-10-07 14:04:24 +0800

    feat: add file module

---
+++ src/file/file.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post } from '@nestjs/common';
+import { FileService } from './file.service';
+
+@Controller('file')
+export class FileController {
+  constructor(private readonly fileService: FileService) {}
+
+  @Post('upload')
+  upload() {
+    return this.fileService.upload();
+  }
+}
